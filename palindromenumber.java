8dfa7a0336fdd233bba5f34d0e79ac4f29301209
Aryaaa-9426 <singhmaksuden@bbdu.ac.in>
2026-06-18 23:55:13 +0530

    merge

---
+++ palindromenumber.java
@@ -0,0 +1,29 @@
+
+
+ public class palindromenumber {
+    
+
+  public static  boolean isPalindrome(int x) {
+
+
+        if (x<0){
+            return false;
+        }
+        int n=x;
+        int  revnum=0;
+        while (n>0){
+            int d= n%10;
+            revnum =revnum*10+d;
+            n=n/10;
+        }
+        if (revnum==x){
+            return true;
+        }
+    }
+} 
+public static void main(String[] args) {
+    int x=121;
+    System.out.println(isPalindrome(x));
+} else {
+    System.out.println(is not Palindrome(x));
+}
