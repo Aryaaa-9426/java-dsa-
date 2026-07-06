a2d40848b0f4847c1036b999eb78861aa279bba9
Aryaaa-9426 <singhmaksuden@bbdu.ac.in>
2026-07-06 21:35:11 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,8 +1,5 @@
-class  Solution {
-
-  public  boolean isPalindrome(int x) {
-
-
+class Solution {
+    public boolean isPalindrome(int x) { 
         if (x<0){
             return false ;
         }
@@ -24,3 +21,5 @@ class  Solution {
 
     }
 }
+        
+    
